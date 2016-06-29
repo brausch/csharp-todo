efe57e09548def1df75261ca0413d102f8cdb94e
Brittany Rausch <berausch247@gmail.com>
2016-06-29 16:57:07 -0700

    To do list csharp

---
+++ Objects/Tasks.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+
+namespace ToDoList.Objects
+{
+  public class Task
+  {
+    private string _description;
+    private static List<string> _instances = new List<string>{};
+    public Task (string description)
+    {
+      _description = description;
+    }
+    public string GetDescription()
+    {
+      return _description;
+    }
+    public void SetDescription(string newDescription)
+    {
+      _description = newDescription;
+    }
+    public static List<string> GetAll()
+    {
+      return _instances;
+    }
+    public void Save()
+    {
+      _instances.Add(this);
+    }
+  }
+}
